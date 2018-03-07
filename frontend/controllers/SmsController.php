c8f3fe726f7477ca61ce724ab19d26bce335bef6
yaecho <dz1401@aliyun.com>
2018-03-07 17:32:33 +0800

    短信发送修改

--- frontend/controllers/SmsController.php
+++ frontend/controllers/SmsController.php
@@ -4,7 +4,6 @@ namespace frontend\controllers;
 
 use common\models\ResumeModel;
 use common\models\SettingModel;
-use frontend\lib\yunpian\lib\SmsOperator;
 use Yii;
 use yii\data\Pagination;
 
@@ -13,105 +12,53 @@ use frontend\models\ResumeForm;
 
 class SmsController extends BaseController
 {
-    public function actionIndex(){
+    public function actionIndex()
+    {
 
         $model = new ResumeModel();
-        $sms['all'] = $model->find()->where(['not_recycling'=>'1'])->count();
-        $sms['is_send'] = $model->find()->where(['not_recycling'=>'1','is_send'=>'1'])->count();
-        $sms['res'] = $model->find()->where(['not_recycling'=>'1', 'res' => '1'])->count();
+        $sms['all'] = $model->find()->where(['not_recycling' => '1'])->count();
+        $sms['is_send'] = $model->find()->where(['not_recycling' => '1', 'is_send' => '1'])->count();
+        $sms['res'] = $model->find()->where(['not_recycling' => '1', 'res' => '1'])->count();
 
         $model = new SettingModel();
         $smsPost = Yii::$app->request->post('sms_templete');
         $smsData = $model::findOne(['name' => 'sms_templete']);
 
-        if(!empty($smsPost)){
-            if (!$smsData){
-                $model->name = 'sms_templete';
-                $model->value = $smsPost;
-            }else{
-                $model = $smsData;
-                $model->value = $smsPost;
-            }
-            if($model->save()){
+        if (!empty($smsPost)) {
+            $smsData->value = $smsPost;
+            if ($smsData->save()) {
                 $this->success('模板保存成功');
-            }else{
+            } else {
                 $this->error('模板保存失败');
             }
         }
 
-        $smsData = $model::findOne(['name' => 'sms_templete']);
-        if (!$smsData){
-            $smsData['value'] = '';
-        }else{
-            $smsData['value'] = $smsData->value;
-        }
+        $sendSms = $model::findOne(['name' => 'send_sms'])->value;
+        $smsData['value'] = $smsData->value ?: '';
 
-        return $this->render('index', ['smsCount' => $sms, 'sms_templete' => $smsData['value']]);
+        return $this->render('index', ['smsCount' => $sms, 'sms_templete' => $smsData['value'], 'send_sms' => $sendSms]);
     }
 
     public function actionPhoneNumber()
     {
         $cond = [];
-        $curPage = Yii::$app->request->get('page',1);
+        $curPage = Yii::$app->request->get('page', 1);
 
         $res = ResumeForm::getList($cond, $curPage, 10);
-        $pages = new Pagination(['totalCount'=>$res['count'], 'pageSize' => $res['pageSize']]);
+        $pages = new Pagination(['totalCount' => $res['count'], 'pageSize' => $res['pageSize']]);
         $res['page'] = $pages;
 
-        return $this->render('phone-number', ['data'=>$res]);
+        return $this->render('phone-number', ['data' => $res]);
     }
 
     public function actionSmsSend()
     {
-        $cache = Yii::$app->cache;
-        $data = $cache->get('is_sending'); 
-        if ($data === false or $data === 0) { 
-            $cache->set('is_sending', 1, 5*60); 
+        $setting = new SettingModel();
+        if ($setting->changeSendSms()) {
+            $this->success('短信发送服务状态修改成功');
+        } else {
+            $this->error('短信发送服务状态修改失败');
         }
-        if ($data === 1) {
-            $this->error('短信发送任务进行中，请稍后5分钟后再试。');
-            return $this->redirect(['index']);
-        }
-
-        ignore_user_abort();//关掉浏览器，PHP脚本也可以继续执行.
-        set_time_limit(300);// 通过set_time_limit(0)可以让程序无限制的执行下去
-
-        require_once (__DIR__ . '/../lib/yunpian/config.php');
-
-        $smsOperator = new SmsOperator();
-
-        $totalCount = 0;
-        $totalFee =0;
-
-        $partNum = 10;
-        $model = new ResumeModel();
-        $count = $model::find()->where(['not_recycling' => '1', 'res' => '0', 'is_send' => '0'])->count();
-        if($count == 0){
-            $this->error('没短信发送，瞎点什么。');
-            $cache->set('is_sending', 0, 5*60);
-            return $this->redirect(['index']);
-        }
-        $pageSize = ceil($count/$partNum);
-
-        for($i=0;$i<$partNum;$i++){
-            $data = ResumeForm::smsData($pageSize);
-            if (empty($data['id'])) continue;
-
-            if (!$data) {
-                $this->error('没有短信需要发送。');
-                $cache->set('is_sending', 0, 5*60);
-                return $this->redirect(['index']);
-            }
-            $result = $smsOperator->multi_send($data['data']);
-            if($result->success){
-                $totalCount += $result->responseData['total_count'];
-                $totalFee += $result->responseData['total_fee'];
-                ResumeForm::updateIsSend($data['id']);
-            }
-        }
-        $cache->set('is_sending', 0, 5*60); 
-        $this->success('发送'.$totalCount.'条短信，花费'.$totalFee.'元。');
-        
         return $this->redirect(['index']);
     }
 
@@ -121,9 +68,9 @@ class SmsController extends BaseController
         $setting = new SettingModel();
         if (!empty($api)) {
             $result = $setting->yunpian($api);
-            if($result) {
+            if ($result) {
                 $this->success('密匙保存成功');
-            }else{
+            } else {
                 $this->error('密匙保存失败');
             }
         }
--- frontend/views/sms/index.php
+++ frontend/views/sms/index.php
@@ -11,13 +11,15 @@ $this->params['breadcrumbs'][] = $this->title;
     <form action="<?=Url::to(['index'])?>" method="post">
         <input type="hidden" name="_csrf-frontend" value="<?php echo Yii::$app->getRequest()->getCsrfToken(); ?>"/>
         <div class="form-group">
-            <label control-label">短信模板</label>
+            <label control-label>短信模板</label>
             <textarea name="sms_templete" class="form-control" rows="3"><?=Html::encode($sms_templete)?></textarea>
         </div>
         <button type="submit" class="btn btn-default">提交保存</button>
     </form>
 
-
+<hr>
+<p style="margin-top: 15px;">短信发送服务状态：<?=Html::encode($send_sms === 'true'?'开启':'关闭')?> </p>
 <p style="margin-top: 15px;">全部：<?=Html::encode($smsCount['all'])?> 已发送：<?=Html::encode($smsCount['is_send'])?>
     已确认：<?=Html::encode($smsCount['res'])?></p>
-<a class="btn btn-default" href="<?=Url::to(['sms-send'])?>">发送</a>
+
+<a class="btn btn-default" href="<?=Url::to(['sms-send'])?>">修改状态</a>
